b9ef582dc203d27040d044c62feab676f9956633
AkshathaHM <akshathahmohan@gmail.com>
2025-10-18 10:42:18 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-
 import Taskform from "./Components/Taskform";
 import TaskList from "./Components/TaskList";
 import Progresstracker from "./Components/Progresstracker";
 import { useEffect, useState } from "react";
 import "./Components/Style.css";
-
 
 export default function App() {
   const [tasks, setTasks] = useState([]);
@@ -28,16 +26,25 @@ export default function App() {
       setTasks(tasks.filter((_, i) => i != index));
   }
 
+  const clearTasks = () => {
+    setTasks([]);
+  }
+
   return(
-    <div>
-      <h1>Task Focus</h1>
-      <p>Our friendly TaskManager</p>
+    <div className="App">
+      <header>
+      <h1 className="title">Task Focus</h1>
+      <p className="tagline">Our friendly TaskManager</p>
+      </header>
       <Taskform addTask = {addTask}/>
       <TaskList tasks = {tasks} 
       updateTask = {updateTask}
       deleteTask = {deleteTask}/>
       <Progresstracker tasks = {tasks}/>
-      <button>Clear all tasks</button>
+
+      {tasks.length>0 && 
+      (<button onClick={clearTasks} className="clear-btn">Clear all tasks</button>)}
+      
     </div>
   )
 }
